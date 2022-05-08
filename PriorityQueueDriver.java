73fbbc5ae423b6aea7960881d564d0e67c08e523
ethan payne <payne.ethan94@gmail.com>
2022-05-08 01:30:41 -0500

    still doing the thing

--- PriorityQueueDriver.java
+++ PriorityQueueDriver.java
@@ -7,15 +7,44 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
+/**
+public static void main(String[] args)
+You should do the following in main
+    1. Instantiate a PriorityQueue<Student>
+    2. Pass your PrioirtyQueue<Student> variable to readInData(...)
+    3. Pass the value true to the Student class method setUsePriority(...)
+    4. Output all the students in priority order (See output example below).
+    5. Instantiate a max heap for Student
+    6. Pass false to the Student class method setKeyIsId(...)
+    7. Pass false to the Student class method setUsePriority(...)
+    8. Pass your Heap<Student> variable to readInData(...)
+    9. Declare an array of Student
+    Pass your Heap<Student> variable to heapSort(...). Assign the return value to your Student array
+    **/ 
+
+/** Output the students sorted by GPA in descending order.
+public static Student[] heapSort(Heap<Student> studentHeap)
+You should do the following in the heapSort method
+    1. Using the clone(...) method in the class Heap, instantiate a new Heap<Student> assigning a copy of heapStudent
+    2. Instantiate an array of Student with the same size as studentHeap
+    3. From your copy of studentHeap, remove each Student placing it in the array
+    4. Return the array
+ */
+
 public class PriorityQueueDriver {
    public static void main(String[] args) {
-      Scanner scnr = new Scanner(System.in);
+       Scanner scnr = new Scanner(System.in);
+       PriorityQueue<Student> pQueueStudent = null;
+       readInData(pQueueStudent);
 
-      /* Type your code here. */
+    
+
+
    }
    
    public static Student[] heapSort(Heap<Student> studentHeap) {
       /* Type your code here. */
+      
    }
    
    private static void readInData(PriorityQueue<Student> students) {
